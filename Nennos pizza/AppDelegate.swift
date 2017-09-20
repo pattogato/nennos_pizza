96b068c7f29027bb127f6de9741fed3bbb142ca3
buddybuild <help@buddybuild.com>
2017-09-20 01:29:08 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Nennos pizza/AppDelegate.swift
+++ Nennos pizza/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Load application window
         window = DIManager.resolve(service: UIWindow.self)
         DIManager.resolve(service: CartManagerProtocol.self).loadPersistedCart()
